f4d13f3a00016e13f03e3c7fe572af981b76209a
Martin <mbuxmann@outlook.com>
2021-12-02 16:25:25 +0200

    .

---
+++ commands/invites.ts
@@ -0,0 +1,72 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { client } from '..';
+import { supabase } from '../database';
+import { paginationEmbed } from '../components/paginationEmbed';
+
+export const data = new SlashCommandBuilder()
+    .setName('invites')
+    .setDescription('List invites that you have received')
+
+export async function execute(interaction: CommandInteraction) {
+    await interaction.deferReply({
+        ephemeral: true
+    });
+
+    const { data, error } = await supabase
+    .from('invites')
+    .select('sender_discord_id, message_id')
+    .eq('receiver_discord_id', interaction.user.id)
+
+    let pages = [];
+    
+    for (const invite of data!) {
+
+        // Getting the inviter's record
+        const { data: inviterData, error: inviterError } = await supabase
+        .from('developers')
+        .select()
+        .eq('discord_id', invite.sender_discord_id)
+
+        if (inviterError != null) {
+            await interaction.editReply({
+                content: 'Something went wrong.',
+            }); 
+
+            return
+        }
+
+        const { 
+            discord_id: inviterDiscordId,
+            timezone: inviterTimezone,
+            discord: inviterDiscord, 
+            skills:inviterSkills, 
+            desired_skills: inviterDesiredSkills, 
+            goal: inviterGoal,
+            position: inviterPosition, 
+            twitter: inviterTwitter, 
+            github: inviterGithub,
+            available: inviterAvailable } = inviterData![0]
+    
+        const user = await client.users?.fetch(inviterDiscordId);
+      
+        const embedMessage = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`${inviterDiscord.charAt(0).toUpperCase() + inviterDiscord.slice(1)}'s profile`)
+        .setThumbnail(user.avatarURL() ?? user.defaultAvatarURL)
+        .setDescription(inviterPosition)
+        .addFields(
+            {name: 'Skills', value: inviterSkills}, 
+            {name: 'Desired Skills', value: inviterDesiredSkills},
+            {name: 'Goal', value: inviterGoal ?? "none"},
+            {name: 'Available', value: inviterAvailable ? 'True' : 'False', inline: true},
+            {name: 'Timezone', value: inviterTimezone, inline: true},
+            {name: 'Github', value: `https://github.com/${inviterGithub}`, inline: true},
+            {name: 'Twitter', value: `https://twitter.com/${inviterTwitter}`, inline: true},
+        )
+    
+        pages.push(embedMessage)   
+    }
+
+    await paginationEmbed(interaction, pages);
+}
--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,24 +1,109 @@
-export const name = 'interactionCreate';
+import { TextChannel, ThreadChannel } from 'discord.js';
+import { supabase } from '../database';
+import {client} from '../index'
+import dotenv from 'dotenv';
+dotenv.config()
 
+export const name = 'interactionCreate';
 export async function execute(interaction: any) {
     if (!interaction.isCommand() && !interaction.isButton()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
-    try {
-        await command.execute(interaction)
-        console.log(`${interaction.user.tag} in #${interaction.channel?.name} triggered an interaction.`);
-    }
-    catch (error) {
-        console.error(error)
-        try {
-          await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-        catch (e) {
-          console.error(e);
-        }
-    }
   
+    // handle interactions that are commands
+    if (interaction.isCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) return;
+        try {
+            await command.execute(interaction)
+            console.log(`${interaction.user.tag} in #${interaction.channel?.name} triggered an interaction.`);
+        }
+        catch (error) {
+            console.error(error)
+            try {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+            catch (e) {
+                console.error(e);
+            }
+        }   
+    }
 
+    // handle interactions that are buttons
+    if (interaction.isButton()) {
+        var starNames = require('@frekyll/star-names')
+        const discordChannel = process.env.DISCORD_CHANNEL_ID
+
+        if (interaction.customId === 'accept'){
+            const { data, error } = await supabase
+            .from('invites')
+            .select()
+            .eq('message_id', interaction.message.id)
+            
+            if (error != null) {
+                await interaction.reply({
+                    content: 'Something went wrong.',
+                    ephemeral: true
+                }); 
+        
+                return
+            }
+       
+            const parentChannel = client.channels.cache.get(discordChannel!);
+            const {sender_discord_id, receiver_discord_id} = data![0]; 
+           
+            // Create a thread and invite users.
+            (parentChannel as TextChannel).threads
+            .create({
+                name: `${starNames.random()}`,
+                autoArchiveDuration: 60,
+                reason: 'Needed a separate thread for pairing',
+            }).then(async (threadChannel: ThreadChannel) => {
+                threadChannel.members.add(sender_discord_id);
+                threadChannel.members.add(receiver_discord_id);
+    
+                // delete invite record
+                const { error } = await supabase
+                .from('invites')
+                .delete()
+                .eq('message_id', interaction.message.id)
+        
+                if (error != null) {
+                    await interaction.reply({
+                        content: 'Something went wrong.',
+                        ephemeral: true
+                    }); 
+            
+                    return;
+                } 
+            }).catch(console.error);
+    
+            // Let the inviter know if the user has accepted.
+            (await client.users.fetch(sender_discord_id)).send({
+                content: `${interaction.user.tag} has accepted your invite!`
+            })
+    
+            await interaction.reply({
+                content: 'Invitation successfully accepted!',
+            });
+        }
+    
+        if (interaction.customId === 'decline'){
+            const { error } = await supabase
+            .from('invites')
+            .delete()
+            .eq('message_id', interaction.message.id)
+    
+            if (error != null) {
+                await interaction.reply({
+                    content: 'Something went wrong.',
+                    ephemeral: true
+                }); 
+        
+                return;
+            }  
+    
+            await interaction.reply({
+                content: 'Invitation successfully declined!',
+            });
+        }
+    }
 }
