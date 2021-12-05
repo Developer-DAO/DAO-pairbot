0a8241e7aaa5c9a1b262f9fc2a903a7b1b90063e
Martin <mbuxmann@outlook.com>
2021-12-05 20:04:44 +0200

    added scheduler to delete archived channel every 12 hours.

--- utils/scheduledJobs.ts
+++ utils/scheduledJobs.ts
@@ -1,30 +1,54 @@
 import { ToadScheduler, SimpleIntervalJob, Task } from 'toad-scheduler';
 import { supabase } from '../database'
 import moment from 'moment';
+import { client } from '../index';
+import dotenv from 'dotenv'
+import { TextBasedChannel, TextChannel, ThreadManager } from 'discord.js';
+
+dotenv.config();
 
 const Scheduler = () => {
-const scheduler = new ToadScheduler();
+    const scheduler = new ToadScheduler();
 
-const task = new Task('Remove expired invites', async () => {
-    console.log('Task triggered');
+    const removeExpiredInvitesTask = new Task('Remove expired invites', async () => {
+        console.log('Removing expired invites.');
+        const { error } = await supabase
+        .from('invites')
+        .delete()
+        .lte('created_at', moment().subtract(1, 'days').format())
 
-    const { error } = await supabase
-    .from('invites')
-    .delete()
-    .lte('created_at', moment().subtract(1, 'days').format())
+        if (error != null) {
+            console.log(error.message);
+        }
+    });
 
-    if (error != null) {
-      console.log(error.message);
-    }
-});
+    const removeExpiredInvites = new SimpleIntervalJob(
+        { hours: 12, runImmediately: true },
+        removeExpiredInvitesTask,
+        'id_1'
+        );
+        
+    const removeArchivedThreadsTask = new Task('Remove archived threads', async () => {
+        console.log('Removing archived channels.')
+        const mainChannel = client.channels.cache.get('913130959069663333');
+        const archivedThreads = await (mainChannel as TextChannel)?.threads.fetchArchived() 
 
-const removeExpiredInvites = new SimpleIntervalJob(
-    { seconds: 30, runImmediately: true },
-    task,
-    'id_1'
-    );
-    
+        if (!archivedThreads) return;
+
+        archivedThreads?.threads.forEach((channel) => {
+            channel.delete(channel.id);
+            console.log(`${channel.name} successfully deleted!`);
+        });
+    })
+        
+
+    const removeArchivedThreads = new SimpleIntervalJob(
+        { hours: 12, runImmediately: true },
+        removeArchivedThreadsTask,
+        'id_2'
+        );
+
     scheduler.addSimpleIntervalJob(removeExpiredInvites);
+    scheduler.addSimpleIntervalJob(removeArchivedThreads);
 }
-    
 export default Scheduler;
