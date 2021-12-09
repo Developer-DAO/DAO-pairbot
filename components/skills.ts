0a7c60c1aca2db62d03e654ceb8adf4c29facf6d
Andrea Cavo <andreacavo92@gmail.com>
2021-12-09 23:37:41 +0100

    Reworked the /add functionality to better choose skills and desired skills.

---
+++ components/skills.ts
@@ -0,0 +1,14 @@
+export const skills = [
+    {'Web3': ['Solidity', 'Solana', 'ZK Development']}, 
+    {'Frontend': ['React', 'Angular', 'Javascript', 'Typescript', 'UI design']},
+    {'Backend': ['Java', 'C#', 'Ruby', 'Python', 'Node.js', 'C++', 'Rust', 'Functional programming']},
+    {'Cloud': ['AWS']}, 
+    {'Database': ['SQL', 'PostgreSQL', 'IPFS/Arweave...']},
+    {'Web3 related': ['Tokenomics', 'DeFi']}, 
+    {'Security': ['Web 3 Security']}, 
+    {'Other': ['Open source', 'GraphQL']}
+]
+
+//skills.forEach((dict: any) => {                   //Filters through skills
+//    for (let elem in dict) {                      //Filters through the dictionary
+//        for (let skill in dict[elem].sort())      //Filters through the values of the dictionary
