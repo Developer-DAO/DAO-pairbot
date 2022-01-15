bffc3b460d7630fba347ad03af01175cd87d1951
Martin <mbuxmann@outlook.com>
2022-01-15 16:02:31 +0200

    Reworked commands and events so that they load on deployment

--- commands/invite.ts
+++ commands/invite.ts
@@ -3,49 +3,54 @@ import { CommandInteraction } from "discord.js";
 import { supabase } from "../database";
 import { inviteDeveloper } from '../components/inviteDeveloper';
 
-export const data = new SlashCommandBuilder()
-  .setName('invite')
-  .setDescription('Invite a developer to pair up with.')
-  .addUserOption(option => option.setRequired(true).setName('developer').setDescription(`Developer's discord tag`))
 
+export const InviteCommand = {
+    name: 'invite',
+    commandJSON: () => {
+        return new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Invite a developer to pair up with.')
+        .addUserOption(option => option.setRequired(true).setName('developer').setDescription(`Developer's discord tag`))    
+    },
+    execute: async (interaction: CommandInteraction) => { 
+        const { options } = interaction;
 
-export async function execute(interaction: CommandInteraction) {
-    const { options } = interaction;
-
-    await interaction.reply({
-        content: `Busy inviting ${options.getUser('developer')?.tag}...`,
-        ephemeral: true
-    });
-
-    // Can't invite yourself
-    if (options.getUser('developer')!.id === interaction.user.id) {
-        await interaction.editReply({
-            content: "Can't invite yourself.",
-        }); 
-
-        return;
-    }
-
-    //Checking inviter
-    const { data: inviterData, error: inviterError } = await supabase
-    .from('developers')
-    .select()
-    .eq('discord_id', interaction.user.id)
-
-    if (inviterError != null) {
-        await interaction.editReply({
-            content: 'Something went wrong.',
-        }); 
-        return;
-    }
-
-    //Inviter not in the developers table
-    if (inviterData?.length === 0) {
-        await interaction.editReply({
-            content: '/Add yourself and make yourself /available before inviting someone to pair with you!',
+        await interaction.reply({
+            content: `Busy inviting ${options.getUser('developer')?.tag}...`,
+            ephemeral: true
         });
-        return;
+    
+        // Can't invite yourself
+        if (options.getUser('developer')!.id === interaction.user.id) {
+            await interaction.editReply({
+                content: "Can't invite yourself.",
+            }); 
+    
+            return;
+        }
+    
+        //Checking inviter
+        const { data: inviterData, error: inviterError } = await supabase
+        .from('developers')
+        .select()
+        .eq('discord_id', interaction.user.id)
+    
+        if (inviterError != null) {
+            await interaction.editReply({
+                content: 'Something went wrong.',
+            }); 
+            return;
+        }
+    
+        //Inviter not in the developers table
+        if (inviterData?.length === 0) {
+            await interaction.editReply({
+                content: '/Add yourself and make yourself /available before inviting someone to pair with you!',
+            });
+            return;
+        }
+    
+        await inviteDeveloper(interaction, options, inviterData);
     }
-
-    await inviteDeveloper(interaction, options, inviterData);
 }
+
--- commands/unavailable.ts
+++ commands/unavailable.ts
@@ -2,27 +2,31 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 import { supabase } from '../database';
 
-export const data = new SlashCommandBuilder()
-  .setName('unavailable')
-  .setDescription('Set your status as unavailable for pairing')
-
-export async function execute(interaction: CommandInteraction) {
-    const error = await supabase
-    .from('developers')
-    .update({ 
-        available: false,
-        goal: null,     
-    }).eq('discord_id', interaction.user.id)
-   
-    let resultMessage = 'Successfully updated status to unavailable!';
-    if (error.status == 404) {
-        resultMessage = 'You are not in the pairing database! \nPlease use the **/add** command, set skills and desired skills to add yourself to the database!'
-    } else if (error.error) {
-        resultMessage = 'Something went wrong.';
+export const UnavailableCommand = {
+    name: 'unavailable',
+    commandJSON: () => {
+        return new SlashCommandBuilder()
+        .setName('unavailable')
+        .setDescription('Set your status as unavailable for pairing')
+    },
+    execute: async (interaction: CommandInteraction) => { 
+        const error = await supabase
+        .from('developers')
+        .update({ 
+            available: false,
+            goal: null,     
+        }).eq('discord_id', interaction.user.id)
+       
+        let resultMessage = 'Successfully updated status to unavailable!';
+        if (error.status == 404) {
+            resultMessage = 'You are not in the pairing database! \nPlease use the **/add** command, set skills and desired skills to add yourself to the database!'
+        } else if (error.error) {
+            resultMessage = 'Something went wrong.';
+        }
+    
+        await interaction.reply({
+            content: resultMessage,
+            ephemeral: true,
+        });
     }
-
-    await interaction.reply({
-        content: resultMessage,
-        ephemeral: true,
-    });
 }
