0be977b274846dac79ae288baa6f3eca4c7a0e73
Martin <mbuxmann@outlook.com>
2022-01-21 23:01:08 +0200

    .

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
                 href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: "Twitter",
+                label: "Twitters",
                 href: "https://twitter.com/docusaurus",
               },
             ],
