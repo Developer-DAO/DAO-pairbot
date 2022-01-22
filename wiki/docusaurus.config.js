6b35a86047e01082569f24a46308442217f16b86
Martin <mbuxmann@outlook.com>
2022-01-22 18:55:29 +0200

    .

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           },
           {
             href: "https://github.com/developer-dao/DAO-pairbot",
-            label: "GitHusb",
+            label: "GitHub",
             position: "right",
           },
         ],
